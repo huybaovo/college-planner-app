2c20309b90ce61011feed531d0f2a4efc2c894ac
jochentamang-dev <tamangj@mail.gvsu.edu>
2023-04-23 22:21:27 -0400

    changesss

--- my-app/src/components/FullCalendar.vue
+++ my-app/src/components/FullCalendar.vue
@@ -56,7 +56,7 @@ export default {
   },
   mounted() {
       let storedEvents = fetchCalendarEvents(db, auth.currentUser?.uid)
-      event
+      event = storedEvents.map()
   },
   methods: {
     handleDateSelect(selectInfo: any) {
